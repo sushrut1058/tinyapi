2054d045f27e72d9b1c5db5174c0c5d9e892a610
sushrut1058 <sushrut1058@github.com>
2025-02-01 13:50:39 +0530

    create table ui fix 2

--- client/src/pages/tables/CreateTablePage.tsx
+++ client/src/pages/tables/CreateTablePage.tsx
@@ -3,9 +3,9 @@ import CreateTable from '../../components/tables/CreateTable';
 
 export const CreateTablePage = () => {
   return (
-    <div className="min-h-screen h-screen bg-gray-900 overflow-auto pt-16 custom-scrollbar">
+    <div className="min-h-screen h-screen bg-gray-900 overflow-auto custom-scrollbar">
       <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-12 ">
-        <div className="rounded-lg shadow p-6">
+        <div className="rounded-lg shadow p-6 pt-20">
           <h1 className="text-2xl font-bold text-gray-100 mb-6">Create New Table</h1>
           <div className="max-h-[80vh] p-2">
           <CreateTable />
