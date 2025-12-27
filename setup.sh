bc9c20a853bf1b7e130b6a5d50f83cb6a1fa517c
Sushrut Hundikar <sushrut@seishintech.dev>
2025-12-27 06:34:02 +0000

    changes in setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,18 @@
-cp -r db /
+sudo apt update
+sudo apt install golang-go
+sudo mkdir -p /db/private /db/mount
+sudo touch /db/private/db.sqlite3 /db/mount/user_tables.sqlite3
+
+sudo apt update
+
+#nginx
+sudo apt install nginx
+sudo rm -r /etc/nginx/sites-enabled/default
+sudo cp tinyapi.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/tinyapi.conf /etc/nginx/sites-enabled/
+sudo systemctl reload nginx
+
+#golang
+sudo apt install golang-go
+
 sudo docker build -t sandbox api_serve/sandbox
